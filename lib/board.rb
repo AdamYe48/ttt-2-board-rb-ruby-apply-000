0aa19b62c865ef69ae996d89347f361e91e9dc72
ADAM YE <adamye48@gmail.com>
2020-07-17 17:47:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -8,26 +8,26 @@ board[0]
 board[1]
 
 # Position 3: Right
-row[2]
+board[2]
 
 # Position 4: Left
-row[0]
+board[0]
 
 # Position 5: Middle
-row[1]
+board[1]
 
 # Position 6: Right
-row[2]
+board[2]
 
 # Position 7: Left
-row[0]
+board[0]
 
 # Position 8: Middle
-row[1]
+board[1]
 
 # Position 9: Right
-row[2]
+board[2]
 
 # Move X to the Postion 2, Middle
-row[1] = "X"
-row #=> [" ", "X", " "]
+board[1] = "X"
+board #=> [" ", "X", " "]
